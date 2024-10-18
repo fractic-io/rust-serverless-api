a1e5e4a53c39bc3ae304ac84caef2ccc2c158d18
Mart van Buren <mart@fractic.io>
2024-10-18 09:15:22 +0900

    [request] Better error messages for request parsing.
    Using the serde_json_path_to_error crate, request parsing error messages
    now include the name of the field that failed to parse.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// This crate is a drop-in replacement for serde_json, but with better error
+// messages (specifically, it mentions the name of the field that failed to
+// parse).
+//
+// For this entire library, remap the serde_json crate to use it instead:
+extern crate serde_json_path_to_error as serde_json;
+
 pub mod auth;
 pub mod errors;
 pub mod macros;
